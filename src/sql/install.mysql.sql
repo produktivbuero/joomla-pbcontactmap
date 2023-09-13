7e7d68a5b9d9a029a0437ef40099335a533919fe
Sebastian Brümmer <sebastian@produktivbuero.de>
2023-09-13 09:30:12 +0200

    Update sql structure

--- src/sql/install.mysql.sql
+++ src/sql/install.mysql.sql
@@ -1,6 +1,6 @@
 # install.mysql.utf8.sql
 
-CREATE TABLE IF NOT EXISTS `#__contact_pbcontactmap` (
+CREATE TABLE `#__contact_pbcontactmap` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `contact_id` int(11) NOT NULL,
   `query` mediumtext NOT NULL COMMENT 'Query made towards OSM',
@@ -11,10 +11,12 @@ CREATE TABLE IF NOT EXISTS `#__contact_pbcontactmap` (
   `boundingbox` mediumtext NOT NULL,
   `lat` float(10,8) NOT NULL,
   `lon` float(11,8) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `display_name` mediumtext NOT NULL,
   `class` varchar(100) NOT NULL,
   `type` varchar(100) NOT NULL,
+  `place_rank` int(11) NOT NULL,
   `importance` float(5,4) NOT NULL,
-  `icon` mediumtext NOT NULL,
+  `addresstype` varchar(100) NOT NULL
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1;
